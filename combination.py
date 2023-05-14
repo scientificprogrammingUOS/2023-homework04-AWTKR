2cb31eac6d1b46e327fbc5980c5e7a445b1fba6a
Crapling <16565731+Crapling@users.noreply.github.com>
2023-05-14 19:45:18 +0200

    fixed error handling again

--- combination.py
+++ combination.py
@@ -7,7 +7,7 @@ def combination(arr1: NDArray, arr2: NDArray, axis=0):
     arr1 = arr1.squeeze()
     arr2 = arr2.squeeze()
     
-    if len(arr1.shape) != len(arr2.shape):
+    if arr1.shape[:axis] != arr2.shape[:axis] or arr1.shape[axis+1:] != arr2.shape[axis+1:]:
         raise ValueError(f"cannot combine arrays along axis {axis}, wrong dimensions")
 
     return np.concatenate((arr1, arr2), axis=axis)
