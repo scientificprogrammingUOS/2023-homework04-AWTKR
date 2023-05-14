d3342bde327163d860b32a3b23773fd4f13faa2e
Marc Meijer <marc.meijer@dfki.de>
2023-05-14 18:58:57 +0200

    completed

--- combination.py
+++ combination.py
@@ -1,13 +1,17 @@
 import numpy as np 
+from numpy.typing import NDArray
 
 # implement your function to combine two numpy arrays 
 
-def combine():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def combination(arr1: NDArray, arr2: NDArray, axis=0):
+    arr1 = arr1.squeeze()
+    arr2 = arr2.squeeze()
+    
+    if not arr1.shape[axis] != arr2.shape[axis]:
+        raise ValueError(f"cannot combine arrays along axis {axis}, wrong dimensions")
+        return 
 
+    return np.concatenate((arr1, arr2), axis=axis)
 
 if __name__ == "__main__":
-    # use this for your own testing!
-
     pass
--- gaussian_analysis.py
+++ gaussian_analysis.py
@@ -2,13 +2,30 @@ import numpy as np
 
 # implement the function gaussian_analysis
 
-def gaussian_analysis():
-        
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def gaussian_analysis(loc, scale, lower_bound, upper_bound):
+    if not (isinstance(loc, int) or isinstance(loc, float)):
+        print(f"type of loc ist wrong")
+        return 
+    if not (isinstance(scale, int) or isinstance(scale, float)):
+        print(f"type of scale ist wrong")
+        return 
+    if not (isinstance(lower_bound, int) or isinstance(lower_bound, float)):
+        print(f"type of lower_bound ist wrong")
+        return 
+    if not (isinstance(upper_bound, int) or isinstance(upper_bound, float)):
+        print(f"type of upper_bound ist wrong")
+        return 
+    if upper_bound <= lower_bound:
+        print(f"upper_bound is smaller or equal to lower_bound")
+        return 
 
+    samples = np.random.normal(loc, scale, 100) 
+    ubounded_samples = samples[samples <= upper_bound]
+    bounded_samples = ubounded_samples[ubounded_samples >= lower_bound] 
+    std = np.std(bounded_samples)
+    mean = np.mean(bounded_samples) 
+    return (mean, std)
 
 if __name__ == "__main__":
-    # use this for your own testing!
+    print(gaussian_analysis(0, 1, 2, 3))
 
-    pass
